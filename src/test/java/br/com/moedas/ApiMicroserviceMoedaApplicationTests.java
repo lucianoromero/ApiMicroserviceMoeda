19b94eedac8784d7aa5d0caa65a1e0f15dd39fa6
Luciano Luiz Romero <40298952+lucianoromero@users.noreply.github.com>
2021-12-21 15:17:56 -0300

    01 Start Inicial do projeto

---
+++ src/test/java/br/com/moedas/ApiMicroserviceMoedaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMicroserviceMoedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
