1b5749cfea22bd7f19d46e0889c5ab0498238e69
Luciano Romero <lucianoluizromero@gmail.com>
2021-12-24 11:03:07 -0300

    02 Implementando CircuitBreaker com Resilience4j

--- src/main/java/br/com/moedas/dto/ResponseDTO.java
+++ src/main/java/br/com/moedas/dto/ResponseDTO.java
@@ -1,7 +1,9 @@
 package br.com.moedas.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
 public class ResponseDTO {
 
 	@JsonProperty("STATUS")
--- src/main/java/br/com/moedas/service/ConsultaMoedasServices.java
+++ src/main/java/br/com/moedas/service/ConsultaMoedasServices.java
@@ -5,22 +5,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.moedas.dto.ResponseDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 public class ConsultaMoedasServices {
 
 	private static final String BASEURL = "http://economia.awesomeapi.com.br/json/last";
+	private ResponseDTO responseDTO;
 
+	@CircuitBreaker(name = "defaul", fallbackMethod = "fallbackMethod")
 	public ResponseDTO Consultar() {
-		ResponseDTO responseDTO = null;
-		ResponseEntity<ResponseDTO> responseEntity = new RestTemplate().getForEntity(BASEURL + "/USD-BRL,EUR-BRL",ResponseDTO.class);
-		if (responseEntity.getStatusCode().is2xxSuccessful()) {
-			responseDTO = responseEntity.getBody();
-			responseDTO.setStatus("Consulta realizada com sucesso");
-			return responseDTO;
-		}
-		responseDTO.setStatus("Verificar os parâmetros");
+		responseDTO = null;
+		ResponseEntity<ResponseDTO> responseEntity = new RestTemplate().getForEntity(BASEURL + "/USD-BRL,EUR-BRL",
+				ResponseDTO.class);
+		responseDTO = responseEntity.getBody();
+		responseDTO.setStatus("Consulta realizada com sucesso");
 		return responseDTO;
 	}
 
+	public ResponseDTO fallbackMethod(Exception ex) {
+		ResponseDTO responseDTO = new ResponseDTO();
+		responseDTO.setStatus("Problemas de conexao com a API- economia.awesomeapi.com.br -");
+		return responseDTO;
+	}
 }
